9d517b7235bebcb806115faa189d78fe413d6297
OUTERGA-MOUSTAFA <outergamoustafa@gmail.com>
2026-03-09 21:39:02 +0000

    BA-8 Configuration du projet Laravel: Installation de Laravel 10/11
    Configuration de la base de données
    
    Installation et configuration de Laravel Sanctum
    
    Configuration des fichiers d'environnement
    
    Mise en place des migrations initiales

---
+++ app/Http/Controllers/Api/AuthController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class AuthController extends Controller
+{
+     public function register(Request $request)
+    {
+
+
+    }
+    public function login(Request $request)
+    {
+
+    
+    }
+    public function logout(Request $request)
+    {
+
+    
+    }
+}
---
+++ routes/api.php
@@ -0,0 +1,10 @@
+<?php
+
+use App\Http\Controllers\Api\AuthController;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
+    return $request->user();
+});
+Route::apiResource('register', AuthController::class);
